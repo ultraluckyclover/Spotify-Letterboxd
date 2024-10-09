5d089d8f3670f6cb13b8440d0f98679c4185036a
Drew Goldstein <goldsteindrew314@gmail.com>
2024-10-09 12:31:52 -0400

    Header, Hero, and Recommended components for landing page

---
+++ spotify-letterboxd-app/src/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import './App.css';
+import Header from './components/Header/Header'
+import Hero from './components/Hero/Hero'
+import Recommended from './components/Recommended/Recommended'
+
+
+
+function App() {
+  return (
+    <div className="app">
+
+        <Header />
+        <Hero/>
+        <Recommended />
+
+      
+        
+      
+      {/* Popular Albums */}
+    </div>
+  );
+}
+
+export default App;
---
+++ spotify-letterboxd-app/src/components/componentsindex.js
@@ -0,0 +1,4 @@
+import Header from "./Header/Header";
+import Hero from "./Hero/Hero";
+
+export {Header, Hero};
